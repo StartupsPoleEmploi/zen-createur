38527cb7f6c3efd9c250b16a61e8dae537978776
François-Xavier Montigny <francoisxavier.montigny@ysocorp.com>
2020-03-23 14:49:58 +0100

    remove old text from autorized page

--- front/src/pages/other/NotAutorized.js
+++ front/src/pages/other/NotAutorized.js
@@ -19,13 +19,6 @@ const LandingText = styled(Typography).attrs({
   paragraph: true,
 })``
 
-const StyleSeparator = styled.div`
-  background-color: #0065db;
-  border-radius: 2px;
-  width: 4.5rem;
-  height: 0.6rem;
-`
-
 export default function NotAutorized({ showIcon = true }) {
   return (
     <StyledNotAutorized>
@@ -36,30 +29,8 @@ export default function NotAutorized({ showIcon = true }) {
         />
       )}
       <LandingText component="h1" style={{ marginBottom: '5rem' }}>
-        Malheureusement, vous n'êtes pas autorisé à utiliser Zen.
+        Malheureusement, le service n'est pas encore ouvert au grand public.
       </LandingText>
-
-      <Typography paragraph style={{ marginBottom: '5rem' }}>
-        <strong>
-          <span aria-label="Seuls">Seul.e.s</span> les{' '}
-          <span aria-label="assistants ou assistantes maternels">
-            assistant.e.s maternel.le.s
-          </span>
-          <br />
-          (hormis les régions suivantes : Bourgogne-Franche-Comté, Centre Val de
-          Loire et les DOM TOM) sont{' '}
-          <span aria-label="autorisés">autorisé.e.s.</span>
-        </strong>
-      </Typography>
-
-      <Typography paragraph variant="subtitle1">
-        Si vous pensez tout de même être éligible pour utiliser Zen.
-        <br />
-        Merci de nous contacter par e-mail{' '}
-        <a href="mailto:zen.00322@pole-emploi.fr">zen.00322@pole-emploi.fr</a>
-      </Typography>
-
-      <StyleSeparator />
     </StyledNotAutorized>
   )
 }
