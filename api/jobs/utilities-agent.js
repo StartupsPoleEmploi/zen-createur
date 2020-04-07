0b90f4a2fb03e4b99b00a90fc2bdee2202095957
Jean-Claude YALAP <jeanclaude.yalap@ysocorp.com>
2020-04-07 10:23:05 +0200

    [CRON] remove cleanOldFiles

--- api/jobs/utilities-agent.js
+++ api/jobs/utilities-agent.js
@@ -3,13 +3,9 @@ const winston = require('../lib/log')
 
 require('../lib/db') // setup db connection
 
-const cleanOldFiles = require('../lib/cleanOldFiles')
 const postDeclarationStatusToSlack = require('../lib/postDeclarationStatusToSlack')
 
 winston.info('Starting utilities agent')
-
-// Every sunday at 2:30
-job('0 30 2 * * 1', cleanOldFiles, null, true, 'Europe/Paris')
 
 
 // Every day at 9, 18
