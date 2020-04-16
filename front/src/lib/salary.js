18d48275a647b3500cc38b01232f19e1ff0ea045
François-Xavier Montigny <francoisxavier.montigny@ysocorp.com>
2020-04-16 14:15:29 +0200

    quick fix

--- front/src/lib/salary.js
+++ front/src/lib/salary.js
@@ -3,10 +3,15 @@ import { isObject } from 'lodash';
 export const WORK_HOURS = 'workHours';
 export const SALARY = 'salary';
 export const TURNOVER = 'turnover';
-export const MIN_SALARY = 0;
+export const WORK_HOURS_CREATOR = 'workHoursCreator';
+export const MIN_SALARY = 1;
 export const MIN_WORK_HOURS = 1;
+export const MIN_WORK_HOURS_CREATOR = 0;
+export const MIN_TURNOVER = 0;
 export const MAX_SALARY = 99999;
 export const MAX_WORK_HOURS = 1000000; // Arbitrary high value so users aren't limited
+export const MAX_WORK_HOURS_CREATOR = 1000000;
+export const MAX_TURNOVER = 1000000;
 
 export const calculateTotal = (employers, field, lowLimit, highLimit) => {
   const total = employers.reduce((prev, employer) => {
