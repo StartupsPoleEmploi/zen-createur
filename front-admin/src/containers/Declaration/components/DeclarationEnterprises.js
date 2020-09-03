46e0198eafc56851929e225534258a1ce3460758
François-Xavier Montigny <francoisxavier.montigny@ysocorp.com>
2020-09-03 11:51:07 +0200

    migrate status of declaration

--- front-admin/src/containers/Declaration/components/DeclarationEnterprises.js
+++ front-admin/src/containers/Declaration/components/DeclarationEnterprises.js
@@ -5,84 +5,78 @@ import moment from 'moment';
 import ZnTable from '../../../components/ZnTable';
 
 function calculateTotal(employers, field) {
-  const sum = employers.reduce(
-    (prev, employer) => parseFloat(employer[field]) + prev,
-    0,
-  );
-  return Math.round(sum);
+	const sum = employers.reduce((prev, employer) => parseFloat(employer[field]) + prev, 0);
+	return Math.round(sum);
 }
 
 const defaultVal = { znSearchable: false, znSort: null, ellipsis: false };
 const COLUMNS_EMPLOYERS = [
-  { title: 'Id', dataIndex: 'id', ...defaultVal },
-  { title: 'CA', dataIndex: 'turnover', ...defaultVal },
-  { title: 'Temps travailler', dataIndex: 'workHours', ...defaultVal },
-  { title: 'Contrat terminé', dataIndex: 'hasEndedThisMonth', ...defaultVal },
-  { title: 'Créé le', dataIndex: 'createdAt', ...defaultVal },
-  { title: 'Modifié le', dataIndex: 'updatedAt', ...defaultVal },
+	{ title: 'Id', dataIndex: 'id', ...defaultVal },
+	{ title: 'Statut', dataIndex: 'status', ...defaultVal },
+	{ title: 'CA', dataIndex: 'turnover', ...defaultVal },
+	{ title: 'Temps travailler', dataIndex: 'workHours', ...defaultVal },
+	{ title: 'Contrat terminé', dataIndex: 'hasEndedThisMonth', ...defaultVal },
+	{ title: 'Créé le', dataIndex: 'createdAt', ...defaultVal },
+	{ title: 'Modifié le', dataIndex: 'updatedAt', ...defaultVal }
 ];
 const COLUMNS_DOCS = [
-  { title: 'Id', dataIndex: 'id', ...defaultVal },
-  { title: 'Type', dataIndex: 'type', ...defaultVal },
-  { title: 'Nom du fichier', dataIndex: 'originalFileName', ...defaultVal },
-  { title: 'Transmis', dataIndex: 'isTransmitted', ...defaultVal },
-  { title: 'Nettoyé', dataIndex: 'isCleanedUp', ...defaultVal },
-  { title: 'Créé le', dataIndex: 'createdAt', ...defaultVal },
-  { title: 'Modifié le', dataIndex: 'updatedAt', ...defaultVal },
-  { title: 'Fichier', dataIndex: 'file', ...defaultVal },
+	{ title: 'Id', dataIndex: 'id', ...defaultVal },
+	{ title: 'Type', dataIndex: 'type', ...defaultVal },
+	{ title: 'Nom du fichier', dataIndex: 'originalFileName', ...defaultVal },
+	{ title: 'Transmis', dataIndex: 'isTransmitted', ...defaultVal },
+	{ title: 'Nettoyé', dataIndex: 'isCleanedUp', ...defaultVal },
+	{ title: 'Créé le', dataIndex: 'createdAt', ...defaultVal },
+	{ title: 'Modifié le', dataIndex: 'updatedAt', ...defaultVal },
+	{ title: 'Fichier', dataIndex: 'file', ...defaultVal }
 ];
 
 type Props = {
-  declaration: Object,
+	declaration: Object
 };
 
 export default function DeclarationEnterprises({ declaration }: Props) {
-  const workHours = calculateTotal(declaration.revenues, 'workHours');
-  const salary = calculateTotal(declaration.revenues, 'turnover');
-  const totalStr = `Total: ${workHours} h, ${salary} €`;
+	const workHours = calculateTotal(declaration.revenues, 'workHours');
+	const salary = calculateTotal(declaration.revenues, 'turnover');
+	const totalStr = `Total: ${workHours} h, ${salary} €`;
 
-  const data = declaration.revenues.map((employer) => ({
-    ...employer,
-    hasEndedThisMonth: employer.hasEndedThisMonth ? 'oui' : 'non',
-    createdAt: moment(employer.createdAt).format('YYYY-MM-DD HH:mm:ss'),
-    updatedAt: moment(employer.updatedAt).format('YYYY-MM-DD HH:mm:ss'),
-  }));
+	const data = declaration.revenues.map((employer) => ({
+		...employer,
+		hasEndedThisMonth: employer.hasEndedThisMonth ? 'oui' : 'non',
+		createdAt: moment(employer.createdAt).format('YYYY-MM-DD HH:mm:ss'),
+		updatedAt: moment(employer.updatedAt).format('YYYY-MM-DD HH:mm:ss')
+	}));
 
-  return (
-    <Card
-      title="Créateur"
-      style={{ marginBottom: '20px' }}
-      extra={<h3>{totalStr}</h3>}
-    >
-      <ZnTable
-        rowKey="id"
-        size="small"
-        style={{ backgroundColor: 'white' }}
-        columns={COLUMNS_EMPLOYERS}
-        dataSource={data}
-        pagination={false}
-        expandedRowRender={(employer) => {
-          const dataDoc = employer.documents.map((doc) => ({
-            ...doc,
-            isTransmitted: doc.isTransmitted ? 'oui' : 'non',
-            isCleanedUp: doc.isCleanedUp ? 'oui' : 'non',
-            createdAt: moment(doc.createdAt).format('YYYY-MM-DD HH:mm:ss'),
-            updatedAt: moment(doc.updatedAt).format('YYYY-MM-DD HH:mm:ss'),
-          }));
-          return (
-            <div style={{ marginTop: '8px', marginBottom: '8px' }}>
-              <ZnTable
-                rowKey="id"
-                size="small"
-                style={{ backgroundColor: 'white' }}
-                columns={COLUMNS_DOCS}
-                dataSource={dataDoc}
-                pagination={false}
-              />
-            </div>
-          );
-        }}
-      />
-    </Card>
-  );
+	return (
+		<Card title="Créateur" style={{ marginBottom: '20px' }} extra={<h3>{totalStr}</h3>}>
+			<ZnTable
+				rowKey="id"
+				size="small"
+				style={{ backgroundColor: 'white' }}
+				columns={COLUMNS_EMPLOYERS}
+				dataSource={data}
+				pagination={false}
+				expandedRowRender={(employer) => {
+					const dataDoc = employer.documents.map((doc) => ({
+						...doc,
+						isTransmitted: doc.isTransmitted ? 'oui' : 'non',
+						isCleanedUp: doc.isCleanedUp ? 'oui' : 'non',
+						createdAt: moment(doc.createdAt).format('YYYY-MM-DD HH:mm:ss'),
+						updatedAt: moment(doc.updatedAt).format('YYYY-MM-DD HH:mm:ss')
+					}));
+					return (
+						<div style={{ marginTop: '8px', marginBottom: '8px' }}>
+							<ZnTable
+								rowKey="id"
+								size="small"
+								style={{ backgroundColor: 'white' }}
+								columns={COLUMNS_DOCS}
+								dataSource={dataDoc}
+								pagination={false}
+							/>
+						</div>
+					);
+				}}
+			/>
+		</Card>
+	);
 }
